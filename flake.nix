f57b508e2d8f81cde665a0da32e59fbad6a4cfb3
Einar Strandberg <einargs@gmail.com>
2023-11-23 15:26:58 -0600

    initial

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Blog built with Rust as the frontend";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+  };
+  nixConfig = {
+    bash-prompt = ''\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]dev-shell:\w]\$\[\033[0m\] '';
+  };
+
+  outputs = { self, nixpkgs }:
+  let system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      tailwindcss = pkgs.nodePackages.tailwindcss;
+      tailwind-watch = pkgs.writeShellApplication {
+        name = "tailwind-watch";
+
+        runtimeInputs = [ tailwindcss ];
+        text = ''
+          tailwindcss -i ./input.css -o ./style/output.css --watch
+        '';
+      };
+  in {
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+      buildInputs = with pkgs; [
+        openssl
+        openssl.dev
+        rustup
+        nodejs_20
+        tailwindcss
+        tailwind-watch
+      ];
+      # To setup you'll need to use rustup to install nightly, then
+      # add the wasm-unknown-unknown target.
+
+      # Then do cargo install trunk cargo-leptos
+
+      # To develop, use cargo leptos watch and tailwind-watch
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+      src = [
+        ./flake.nix
+        ./flake.lock
+      ];
+      shellHook = ''
+        alias trunk="~/.cargo/bin/trunk"
+      '';
+    };
+  };
+}
